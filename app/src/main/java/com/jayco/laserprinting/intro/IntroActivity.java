6bc960e1d02ecb8e4f59161dc15dacee310aa1e2
tpriez <dudqja189@gmail.com>
2016-07-15 15:02:27 +0900

    레이아웃 버튼 생성, 인트로 색 변경

--- app/src/main/java/com/jayco/laserprinting/intro/IntroActivity.java
+++ app/src/main/java/com/jayco/laserprinting/intro/IntroActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
-import android.widget.TextView;
 
 import com.jayco.laserprinting.MainActivity;
 import com.jayco.laserprinting.R;
