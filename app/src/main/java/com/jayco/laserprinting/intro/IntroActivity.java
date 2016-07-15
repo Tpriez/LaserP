25ea15fff05d40c7b5bf51fd262618728b8eb4e1
tpriez <dudqja189@gmail.com>
2016-07-15 14:36:04 +0900

    인트로 생성

---
+++ app/src/main/java/com/jayco/laserprinting/intro/IntroActivity.java
@@ -0,0 +1,38 @@
+package com.jayco.laserprinting.intro;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
+import android.widget.TextView;
+
+import com.jayco.laserprinting.MainActivity;
+import com.jayco.laserprinting.R;
+
+/**
+ * Created by FX603 on 2016-07-15.
+ */
+public class IntroActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+
+        init();
+    }
+
+    private void init() {
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                finish();
+                Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        };
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
