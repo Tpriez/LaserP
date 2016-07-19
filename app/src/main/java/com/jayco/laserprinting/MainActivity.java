e6e06e896cfaf5e7d44e128e93a9166abab96aa7
jeong98 <rjt2900@gmail.com>
2016-07-19 10:08:50 +0700

    예제

--- app/src/main/java/com/jayco/laserprinting/MainActivity.java
+++ app/src/main/java/com/jayco/laserprinting/MainActivity.java
@@ -1,7 +1,8 @@
 package com.jayco.laserprinting;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        LinearLayout container = (LinearLayout)findViewById (R.id.container);
+        myView view = new myView (this);
+        container.addView(view);
     }
 }
